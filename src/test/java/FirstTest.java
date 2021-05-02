b4cb48ac74820b19650ffa0d36e77fd256d0a896
tzhvania44 <tzhvania44@gmail.com>
2021-05-02 18:57:07 -0400

    Homework4

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+
+
+
+public class FirstTest {
+
+    @Test
+    public void test() {
+        Configuration.browser = "chrome";
+        System.setProperty("webdriver.chrome.driver", "files/chromedriver.exe");
+        Configuration.startMaximized = true;
+
+        Selenide.open("https://www.google.com/");
+        $(".gLFyf").setValue("Tako Zhvania").sendKeys(Keys.ENTER);
+        sleep(5000);
+
+
+    }
+}
+
+
+
